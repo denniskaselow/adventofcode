cf3777bec64a389e2aeeb8fe9069a0e5655375df
Dennis Kaselow <dennis.kaselow+git@gmail.com>
2022-12-04 21:48:20 +0100

    improve day 4

--- lib/src/2022/day04.dart
+++ lib/src/2022/day04.dart
@@ -18,14 +18,7 @@ day4star1(String input) {
 day4star2(String input) {
   return _processInput(input)
       .map((e) => e.split(',').map((e) => e.split('-').map(int.parse)))
-      .where((element) =>
-          element.first.first <= element.last.first &&
-              element.first.last >= element.last.first ||
-          element.first.first <= element.last.last &&
-              element.first.first >= element.last.first ||
-          element.first.first >= element.last.first &&
-              element.first.last <= element.last.last ||
-          element.first.first <= element.last.first &&
-              element.first.last >= element.last.last)
+      .where((element) => !(element.first.first > element.last.last ||
+          element.first.last < element.last.first))
       .length;
 }
