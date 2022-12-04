efe06649cacc6c0030406dd97504f62390da9950
Dennis Kaselow <dennis.kaselow+git@gmail.com>
2022-12-04 06:18:35 +0100

    day 4

--- lib/src/2022/day04.dart
+++ lib/src/2022/day04.dart
@@ -5,9 +5,27 @@ Iterable<String> _processInput(String input) {
 }
 
 day4star1(String input) {
-  return _processInput(input);
+  return _processInput(input)
+      .map((e) => e.split(',').map((e) => e.split('-').map(int.parse)))
+      .where((element) =>
+          element.first.first >= element.last.first &&
+              element.first.last <= element.last.last ||
+          element.first.first <= element.last.first &&
+              element.first.last >= element.last.last)
+      .length;
 }
 
 day4star2(String input) {
-  return _processInput(input);
+  return _processInput(input)
+      .map((e) => e.split(',').map((e) => e.split('-').map(int.parse)))
+      .where((element) =>
+          element.first.first <= element.last.first &&
+              element.first.last >= element.last.first ||
+          element.first.first <= element.last.last &&
+              element.first.first >= element.last.first ||
+          element.first.first >= element.last.first &&
+              element.first.last <= element.last.last ||
+          element.first.first <= element.last.first &&
+              element.first.last >= element.last.last)
+      .length;
 }
--- test/2022/day04_test.dart
+++ test/2022/day04_test.dart
@@ -2,12 +2,17 @@ import 'package:adventofcode2022/adventofcode2022.dart';
 import 'package:test/test.dart';
 
 void main() {
-  final input = '''''';
+  final input = '''2-4,6-8
+2-3,4-5
+5-7,7-9
+2-8,3-7
+6-6,4-6
+2-6,4-8''';
 
   test('first star', () {
-    expect(day4star1(input), equals(157));
+    expect(day4star1(input), equals(2));
   });
   test('second star', () {
-    expect(day4star2(input), equals(70));
+    expect(day4star2(input), equals(4));
   });
 }
