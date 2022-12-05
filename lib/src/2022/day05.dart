f2dab57dcf35996012f4bfbee70a0d00605c5e28
Dennis Kaselow <dennis.kaselow+git@gmail.com>
2022-12-05 08:23:54 +0100

    day 5

--- lib/src/2022/day05.dart
+++ lib/src/2022/day05.dart
@@ -4,10 +4,61 @@ Iterable<String> _processInput(String input) {
   return input.split('\n');
 }
 
-int day5star1(String input) {
-  return _processInput(input).length;
+String day5star1(String input) {
+  return _run(input, move1);
 }
 
-int day5star2(String input) {
-  return _processInput(input).length;
+String day5star2(String input) {
+  return _run(input, move2);
+}
+
+String _run(
+    String input,
+    void Function(
+            List<List<String>> rows, int amount, int fromColumn, int toColumn)
+        move) {
+  final lines = _processInput(input);
+  final stackCount = (lines.first.length + 1) ~/ 4;
+  final stacks = List<List<String>>.generate(stackCount, (index) => <String>[]);
+  var isStack = true;
+  for (final line in lines) {
+    if (line.isEmpty) {
+      isStack = false;
+      continue;
+    }
+    if (isStack) {
+      final items = List.generate(stackCount,
+          (index) => line.substring(index * 4 + 1, index * 4 + 2).trim());
+      items.forEachIndexed((index, element) {
+        if (element.isNotEmpty) {
+          stacks[index].insert(0, element);
+        }
+      });
+    } else {
+      final command = RegExp(r'(\d+)')
+          .allMatches(line)
+          .map((e) => int.parse(e[0]!))
+          .toList();
+      final amount = command[0];
+      final from = command[1] - 1;
+      final to = command[2] - 1;
+      move(stacks, amount, from, to);
+    }
+  }
+  return List.generate(stackCount, (index) => stacks[index].last).join();
+}
+
+void move1(
+    List<List<String>> stacks, int amount, int fromColumn, int toColumn) {
+  for (var i = 0; i < amount; i++) {
+    stacks[toColumn].add(stacks[fromColumn].removeLast());
+  }
+}
+
+void move2(
+    List<List<String>> stacks, int amount, int fromColumn, int toColumn) {
+  stacks[toColumn].addAll(stacks[fromColumn]
+      .getRange(stacks[fromColumn].length - amount, stacks[fromColumn].length));
+  stacks[fromColumn].removeRange(
+      stacks[fromColumn].length - amount, stacks[fromColumn].length);
 }
--- test/2022/day05_test.dart
+++ test/2022/day05_test.dart
@@ -2,12 +2,20 @@ import 'package:adventofcode2022/adventofcode2022.dart';
 import 'package:test/test.dart';
 
 void main() {
-  final input = '''''';
+  final input = '''    [D]    
+[N] [C]    
+[Z] [M] [P]
+ 1   2   3 
 
-  test('first star', () {
-    expect(day5star1(input), equals(2));
+move 1 from 2 to 1
+move 3 from 1 to 3
+move 2 from 2 to 1
+move 1 from 1 to 2''';
+
+  test('day 5 first star', () {
+    expect(day5star1(input), equals('CMZ'));
   });
-  test('second star', () {
-    expect(day5star2(input), equals(4));
+  test('day 5 second star', () {
+    expect(day5star2(input), equals('MCD'));
   });
 }
