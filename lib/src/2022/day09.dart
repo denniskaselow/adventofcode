db2067e50eb5b1659d54f24045466fa0b33be794
Dennis Kaselow <dennis.kaselow+git@gmail.com>
2022-12-09 09:04:08 +0100

    day 9

--- lib/src/2022/day09.dart
+++ lib/src/2022/day09.dart
@@ -1,13 +1,84 @@
+import 'dart:math';
+
 import 'package:collection/collection.dart';
 
-Iterable<String> _processInput(String input) {
-  return input.split('\n');
+int day9star1(String input) => evaluate(input, 1);
+int day9star2(String input) => evaluate(input, 9);
+
+int evaluate(String input, int tailLength) {
+  final movements = input.split('\n').map((e) => e.split(' '));
+  var x = 0;
+  var y = 0;
+  var minX = 0;
+  var maxX = 0;
+  var minY = 0;
+  var maxY = 0;
+  for (final movement in movements) {
+    final tmp = int.parse(movement[1]);
+    if (movement[0] == 'R') {
+      x += tmp;
+    } else if (movement[0] == 'L') {
+      x -= tmp;
+    } else if (movement[0] == 'U') {
+      y += tmp;
+    } else {
+      y -= tmp;
+    }
+    minX = min(x, minX);
+    minY = min(y, minY);
+    maxX = max(x, maxX);
+    maxY = max(y, maxY);
+  }
+  final grid = List.generate(maxX - minX + 1,
+      (index) => List.generate(maxY - minY + 1, (index) => false));
+  final startX = -minX;
+  final startY = -minY;
+  grid[startX][startY] = true;
+  final rope = Rope(startX, startY, tailLength);
+  for (final movement in movements) {
+    move(grid, movement, rope);
+  }
+  return grid.flattened.where((element) => element).length;
 }
 
-int day9star1(String input) {
-  return _processInput(input).length;
+void move(List<List<bool>> grid, List<String> movement, Rope rope) {
+  final tmp = int.parse(movement[1]);
+  for (var i = 0; i < tmp; i++) {
+    if (movement[0] == 'R') {
+      rope.xHead += 1;
+    } else if (movement[0] == 'L') {
+      rope.xHead -= 1;
+    } else if (movement[0] == 'U') {
+      rope.yHead += 1;
+    } else {
+      rope.yHead -= 1;
+    }
+    var nextXPos = rope.xHead;
+    var nextYPos = rope.yHead;
+    for (var j = 0; j < rope.xTail.length; j++) {
+      if ((nextXPos - rope.xTail[j]).abs() > 1 ||
+          (nextYPos - rope.yTail[j]).abs() > 1) {
+        final deltaX = nextXPos - rope.xTail[j];
+        final deltaY = nextYPos - rope.yTail[j];
+        rope.xTail[j] += deltaX.sign;
+        rope.yTail[j] += deltaY.sign;
+        nextXPos = rope.xTail[j];
+        nextYPos = rope.yTail[j];
+      } else {
+        break;
+      }
+    }
+    grid[rope.xTail.last][rope.yTail.last] = true;
+  }
 }
 
-int day9star2(String input) {
-  return _processInput(input).length;
+class Rope {
+  int xHead;
+  int yHead;
+  List<int> xTail;
+  List<int> yTail;
+
+  Rope(this.xHead, this.yHead, int tailLength)
+      : xTail = List.filled(tailLength, xHead),
+        yTail = List.filled(tailLength, yHead);
 }
--- test/2022/day09_test.dart
+++ test/2022/day09_test.dart
@@ -2,12 +2,30 @@ import 'package:adventofcode2022/adventofcode2022.dart';
 import 'package:test/test.dart';
 
 void main() {
-  final input = r'''''';
+  final input = r'''R 4
+U 4
+L 3
+D 1
+R 4
+D 1
+L 5
+R 2''';
 
   test('day 9 first star', () {
-    expect(day9star1(input), equals(0));
+    expect(day9star1(input), equals(13));
   });
   test('day 9 second star', () {
-    expect(day9star2(input), equals(0));
+    expect(day9star2(input), equals(1));
+  });
+  test('day 9 second star 2', () {
+    final input2 = '''R 5
+U 8
+L 8
+D 3
+R 17
+D 10
+L 25
+U 20''';
+    expect(day9star2(input2), equals(36));
   });
 }
