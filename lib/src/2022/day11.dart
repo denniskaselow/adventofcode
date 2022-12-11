9cb0059bc2f87c129a0ea797c679ede5efbb24c5
Dennis Kaselow <dennis.kaselow+git@gmail.com>
2022-12-11 08:50:08 +0100

    day 11

--- lib/src/2022/day11.dart
+++ lib/src/2022/day11.dart
@@ -1,13 +1,58 @@
 import 'package:collection/collection.dart';
 
-int day11star1(String input) {
-  return _processInput(input).length;
+typedef ManageWorryLevel = int Function(int, int);
+
+int day11star1(String input) => executeRounds(input, 20, (x, _) => x ~/ 3);
+int day11star2(String input) =>
+    executeRounds(input, 10000, (x, maxWorry) => x % maxWorry);
+
+int executeRounds(String input, int rounds, ManageWorryLevel manageWorryLevel) {
+  final monkeys = _processInput(input);
+  final inspections = List.filled(monkeys.length, 0);
+  var maxWorry = monkeys.fold(1, (value, element) => value * element[2].first);
+  for (var round = 0; round < rounds; round++) {
+    for (var i = 0; i < monkeys.length; i++) {
+      final monkey = monkeys[i];
+      for (final item in monkey[0]) {
+        final op = monkey[1];
+        final divisibleBy = monkey[2][0];
+        final ifTrue = monkey[3][0];
+        final ifFalse = monkey[4][0];
+        final v1 = op[1] == 0 ? item : op[1];
+        final worryLevel =
+            manageWorryLevel(op[0] == 0 ? item * v1 : item + v1, maxWorry);
+        final target = worryLevel % divisibleBy == 0 ? ifTrue : ifFalse;
+        monkeys[target.toInt()][0].add(worryLevel);
+        inspections[i]++;
+      }
+      monkey[0].clear();
+    }
+  }
+  inspections.sort((a, b) => b - a);
+  return inspections[0] * inspections[1];
 }
 
-int day11star2(String input) {
-  return _processInput(input).length;
-}
-
-Iterable<String> _processInput(String input) {
-  return input.split('\n');
-}
+List<List<List<int>>> _processInput(String input) => input
+    .replaceAll(',', '')
+    .split('\n\n')
+    .map((e) => e
+        .split('\n')
+        .skip(1)
+        .map((e) => e.split(':')[1].trim().split(' '))
+        .mapIndexed((index, element) => index == 1
+            ? element
+                .skip(3)
+                .map((e) => e == '*'
+                    ? '0'
+                    : e == '+'
+                        ? '1'
+                        : e == 'old'
+                            ? '0'
+                            : e)
+                .toList()
+            : index > 1
+                ? [element.last]
+                : element)
+        .map((e) => e.map(int.parse).toList())
+        .toList())
+    .toList();
--- test/2022/day11_test.dart
+++ test/2022/day11_test.dart
@@ -2,12 +2,38 @@ import 'package:adventofcode2022/adventofcode2022.dart';
 import 'package:test/test.dart';
 
 void main() {
-  final input = r'''''';
+  final input = r'''Monkey 0:
+  Starting items: 79, 98
+  Operation: new = old * 19
+  Test: divisible by 23
+    If true: throw to monkey 2
+    If false: throw to monkey 3
+
+Monkey 1:
+  Starting items: 54, 65, 75, 74
+  Operation: new = old + 6
+  Test: divisible by 19
+    If true: throw to monkey 2
+    If false: throw to monkey 0
+
+Monkey 2:
+  Starting items: 79, 60, 97
+  Operation: new = old * old
+  Test: divisible by 13
+    If true: throw to monkey 1
+    If false: throw to monkey 3
+
+Monkey 3:
+  Starting items: 74
+  Operation: new = old + 3
+  Test: divisible by 17
+    If true: throw to monkey 0
+    If false: throw to monkey 1''';
 
   test('day 11 first star', () {
-    expect(day11star1(input), equals(0));
+    expect(day11star1(input), equals(10605));
   });
   test('day 11 second star', () {
-    expect(day11star2(input), equals(0));
+    expect(day11star2(input), equals(2713310158));
   });
 }
