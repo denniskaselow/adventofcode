97edf6d44d86ef19253a17c6e1ea9c635682ca0b
Dennis Kaselow <dennis.kaselow+git@gmail.com>
2022-12-12 18:34:45 +0100

    prepare day 13

---
+++ lib/src/2022/day13.dart
@@ -0,0 +1,5 @@
+int day13star1(String input) => _processInput(input).length;
+
+int day13star2(String input) => _processInput(input).length;
+
+Iterable<String> _processInput(String input) => input.split('\n');
---
+++ test/2022/day13_test.dart
@@ -0,0 +1,13 @@
+import 'package:adventofcode2022/adventofcode2022.dart';
+import 'package:test/test.dart';
+
+void main() {
+  const input = '''''';
+
+  test('day 13 first star', () {
+    expect(day12star1(input), equals(0));
+  });
+  test('day 13 second star', () {
+    expect(day12star2(input), equals(0));
+  });
+}
