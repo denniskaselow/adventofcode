46605470726b1d9e5358d226750242719d4ad9be
Dennis Kaselow <dennis.kaselow+git@gmail.com>
2022-12-13 07:58:08 +0100

    day 13

--- lib/src/2022/day13.dart
+++ lib/src/2022/day13.dart
@@ -1,5 +1,71 @@
-int day13star1(String input) => _processInput(input).length;
+import 'dart:convert';
+import 'dart:math';
 
-int day13star2(String input) => _processInput(input).length;
+import 'package:collection/collection.dart';
 
-Iterable<String> _processInput(String input) => input.split('\n');
+int day13star1(String input) {
+  final pairs = _processInput(input);
+  final correctIndex = <int>[];
+  var index = 1;
+  for (final pair in pairs) {
+    final left = jsonDecode(pair.first) as List;
+    final right = jsonDecode(pair.last) as List;
+    if (compare(left, right) < 0) {
+      correctIndex.add(index);
+    }
+    index++;
+  }
+  return correctIndex.sum;
+}
+
+int day13star2(String input) {
+  final pairs = _processInput(input);
+  final all = [];
+  for (final pair in pairs) {
+    final left = jsonDecode(pair.first) as List;
+    final right = jsonDecode(pair.last) as List;
+    all.addAll([left, right]);
+  }
+  const marker1 = [
+    [2]
+  ];
+  const marker2 = [
+    [6]
+  ];
+  all.addAll([marker1, marker2]);
+  final sorted = all.sorted((a, b) => compare(a, b));
+  final result = sorted
+      .mapIndexed((index, element) => [index + 1, element])
+      .where((element) => element[1] == marker1 || element[1] == marker2);
+  return (result.first.first as int) * (result.last.first as int);
+}
+
+int compare(List<dynamic> left, List<dynamic> right) {
+  var result = 0;
+  for (var i = 0; i < max(left.length, right.length); i++) {
+    if (i == left.length && i < right.length) {
+      result = -1;
+    } else if (i <= left.length && i == right.length) {
+      result = 1;
+    } else if (left[i] is int && right[i] is int) {
+      if (left[i] < right[i]) {
+        result = -1;
+      } else if (left[i] > right[i]) {
+        result = 1;
+      }
+    } else if (left[i] is int && right[i] is List) {
+      result = compare([left[i]], right[i]);
+    } else if (left[i] is List && right[i] is int) {
+      result = compare(left[i], [right[i]]);
+    } else {
+      result = compare(left[i], right[i]);
+    }
+    if (result != 0) {
+      return result;
+    }
+  }
+  return result;
+}
+
+List<List<String>> _processInput(String input) =>
+    input.split('\n\n').map((e) => e.split('\n').toList()).toList();
--- test/2022/day13_test.dart
+++ test/2022/day13_test.dart
@@ -2,12 +2,35 @@ import 'package:adventofcode2022/adventofcode2022.dart';
 import 'package:test/test.dart';
 
 void main() {
-  const input = '''''';
+  const input = '''
+[1,1,3,1,1]
+[1,1,5,1,1]
+
+[[1],[2,3,4]]
+[[1],4]
+
+[9]
+[[8,7,6]]
+
+[[4,4],4,4]
+[[4,4],4,4,4]
+
+[7,7,7,7]
+[7,7,7]
+
+[]
+[3]
+
+[[[]]]
+[[]]
+
+[1,[2,[3,[4,[5,6,7]]]],8,9]
+[1,[2,[3,[4,[5,6,0]]]],8,9]''';
 
   test('day 13 first star', () {
-    expect(day12star1(input), equals(0));
+    expect(day13star1(input), equals(13));
   });
   test('day 13 second star', () {
-    expect(day12star2(input), equals(0));
+    expect(day13star2(input), equals(140));
   });
 }
