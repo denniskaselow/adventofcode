bd881646786cb51d2c091da6e6f4a6ca4363667d
Dennis Kaselow <dennis.kaselow+git@gmail.com>
2022-12-14 08:00:24 +0100

    day 14

--- lib/src/2022/day14.dart
+++ lib/src/2022/day14.dart
@@ -1,5 +1,64 @@
-int day14star1(String input) => _processInput(input).length;
+import 'dart:math';
 
-int day14star2(String input) => _processInput(input).length;
+import 'package:more/more.dart';
 
-Iterable<String> _processInput(String input) => input.split('\n');
+int day14star1(String input) {
+  final grid = _processInput(input);
+  final maxY = grid.keys.fold(0, max);
+  return simulateSand(grid, maxY, hasFloor: false);
+}
+
+int day14star2(String input) {
+  final grid = _processInput(input);
+  final maxY = grid.keys.fold(0, max) + 1;
+  return simulateSand(grid, maxY, hasFloor: true);
+}
+
+int simulateSand(Map<int, Set<int>> grid, int maxY, {required bool hasFloor}) {
+  var currentX = 500;
+  var currentY = 0;
+  final directions = [
+    [0, 1],
+    [-1, 1],
+    [1, 1]
+  ];
+  var settledCount = 0;
+  while (hasFloor ? !(grid[0]?.contains(500) ?? false) : currentY <= maxY) {
+    var settled = true;
+    for (final direction in directions) {
+      if (!(grid[currentY + direction[1]]?.contains(currentX + direction[0]) ??
+          false)) {
+        currentY = currentY + direction[1];
+        currentX = currentX + direction[0];
+        settled = false;
+        break;
+      }
+    }
+    if (settled || hasFloor && currentY == maxY) {
+      grid.putIfAbsent(currentY, () => <int>{}).add(currentX);
+      currentX = 500;
+      currentY = 0;
+      settledCount++;
+    }
+  }
+  return settledCount;
+}
+
+Map<int, Set<int>> _processInput(String input) => input
+        .split('\n')
+        .map((e) =>
+            e.split(' -> ').map((e) => e.split(',').map(int.parse)).window(2))
+        .fold(<int, Set<int>>{}, (previousValue, element) {
+      for (final cell in element) {
+        final distX = cell[1].first - cell[0].first;
+        final distY = cell[1].last - cell[0].last;
+        for (var x = 0; x <= distX.abs(); x++) {
+          for (var y = 0; y <= distY.abs(); y++) {
+            previousValue
+                .putIfAbsent(cell[0].last + y * distY.sign, () => {})
+                .add(cell[0].first + x * distX.sign);
+          }
+        }
+      }
+      return previousValue;
+    });
--- test/2022/day14_test.dart
+++ test/2022/day14_test.dart
@@ -2,12 +2,14 @@ import 'package:adventofcode2022/adventofcode2022.dart';
 import 'package:test/test.dart';
 
 void main() {
-  const input = '''''';
+  const input = '''
+498,4 -> 498,6 -> 496,6
+503,4 -> 502,4 -> 502,9 -> 494,9''';
 
   test('day 14 first star', () {
-    expect(day14star1(input), equals(0));
+    expect(day14star1(input), equals(24));
   });
   test('day 14 second star', () {
-    expect(day14star2(input), equals(0));
+    expect(day14star2(input), equals(93));
   });
 }
