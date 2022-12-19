30e51f9cc85c05abdcdb12bdf4948450d52fd216
Dennis Kaselow <dennis.kaselow+git@gmail.com>
2022-12-19 18:20:05 +0100

    day 19

--- lib/src/2022/day19.dart
+++ lib/src/2022/day19.dart
@@ -1,13 +1,117 @@
-import 'package:collection/collection.dart';
-import 'package:more/more.dart';
-import '../utils.dart';
+import 'dart:math';
+
+final _regExp = RegExp(
+    r'Blueprint (?<id>\d+): Each ore robot costs (?<ore>\d+) ore. Each clay robot costs (?<clay>\d+) ore. Each obsidian robot costs (?<obsidianOre>\d+) ore and (?<obsidianClay>\d+) clay. Each geode robot costs (?<geodeOre>\d+) ore and (?<geodeObsidian>\d+) obsidian.');
+
+int counter = 0;
 
 int day19star1(String input) {
-  return _processInput(input).length;
+  final blueprints = _processInput(input);
+  var result = 0;
+  for (final blueprint in blueprints) {
+    harvestGeodes(blueprint);
+    result += blueprint.id * blueprint.maxGeodes;
+  }
+  return result;
 }
 
 int day19star2(String input) {
-  return _processInput(input).length;
+  const result = 0;
+  // too slow
+  // final blueprints = _processInput(input);
+  // var result = 1;
+  // for (final blueprint in blueprints.take(3)) {
+  //   counter = 0;
+  //   harvestGeodes(blueprint, 32);
+  //   result *= blueprint.maxGeodes;
+  //   print('$counter paths');
+  //   print('${blueprint.id} - maxGeodes: ${blueprint.maxGeodes}');
+  // }
+  // return result;
+  return result;
 }
 
-_processInput(String input) => input.split('\n');
+Iterable<Blueprint> _processInput(String input) => input
+    .split('\n')
+    .map((e) => _regExp.allMatches(e).first)
+    .map((e) => Blueprint(int.parse(e.namedGroup('id')!), [
+          [int.parse(e.namedGroup('ore')!), 0, 0],
+          [int.parse(e.namedGroup('clay')!), 0, 0],
+          [
+            int.parse(e.namedGroup('obsidianOre')!),
+            int.parse(e.namedGroup('obsidianClay')!),
+            0
+          ],
+          [
+            int.parse(e.namedGroup('geodeOre')!),
+            0,
+            int.parse(e.namedGroup('geodeObsidian')!)
+          ]
+        ]));
+
+void harvestGeodes(Blueprint blueprint,
+    [int timeLeft = 24,
+    List<int> res = const [0, 0, 0, 0],
+    List<int> bots = const [1, 0, 0, 0]]) {
+  final harvest = bots.toList();
+  blueprint.maxGeodes = max(blueprint.maxGeodes, res[3]);
+  if (timeLeft > 0) {
+    var bot = 3;
+    final possibleObsidianBots = min(
+            timeLeft,
+            min(
+                (res[1] + harvest[1] + timeLeft * (bots[1] + timeLeft / 2)) /
+                    blueprint.costs[2][1],
+                (res[0] + harvest[0] + timeLeft * (bots[0] + timeLeft / 2)) /
+                    blueprint.costs[2][0])) ~/
+        2;
+    final possibleGeodeBots = min(
+            timeLeft,
+            (res[2] +
+                    harvest[2] +
+                    timeLeft * (bots[2] + possibleObsidianBots)) ~/
+                blueprint.costs[3][2]) /
+        2;
+    if (res[3] + harvest[3] + timeLeft * (bots[3] + possibleGeodeBots) >
+        blueprint.maxGeodes) {
+      for (final cost in blueprint.costs.reversed) {
+        if (res[0] >= cost[0] && res[1] >= cost[1] && res[2] >= cost[2]) {
+          final nextBots = bots.toList();
+          nextBots[bot]++;
+          harvestGeodes(
+              blueprint,
+              timeLeft - 1,
+              [
+                res[0] - cost[0] + harvest[0],
+                res[1] - cost[1] + harvest[1],
+                res[2] - cost[2] + harvest[2],
+                res[3] + harvest[3]
+              ],
+              nextBots);
+        }
+        bot--;
+      }
+      harvestGeodes(
+          blueprint,
+          timeLeft - 1,
+          [
+            res[0] + harvest[0],
+            res[1] + harvest[1],
+            res[2] + harvest[2],
+            res[3] + harvest[3]
+          ],
+          bots);
+    }
+  }
+  counter++;
+  // if (counter % 100000000 == 0) {
+  //   print('$counter ~~~ ${blueprint.id} => $bots harvested $res');
+  // }
+}
+
+class Blueprint {
+  final int id;
+  final List<List<int>> costs;
+  int maxGeodes = 0;
+  Blueprint(this.id, this.costs);
+}
--- test/2022/day19_test.dart
+++ test/2022/day19_test.dart
@@ -3,12 +3,13 @@ import 'package:test/test.dart';
 
 void main() {
   const input = '''
-''';
+Blueprint 1: Each ore robot costs 4 ore. Each clay robot costs 2 ore. Each obsidian robot costs 3 ore and 14 clay. Each geode robot costs 2 ore and 7 obsidian.
+Blueprint 2: Each ore robot costs 2 ore. Each clay robot costs 3 ore. Each obsidian robot costs 3 ore and 8 clay. Each geode robot costs 3 ore and 12 obsidian.''';
 
   test('day 19 first star', () {
-    expect(day19star1(input), equals(0));
+    expect(day19star1(input), equals(33));
   });
   test('day 19 second star', () {
-    expect(day19star2(input), equals(0));
+    expect(day19star2(input), equals(56 * 62));
   });
 }
