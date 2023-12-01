8b84b8da6e7f9e55e570142a9a3b0b040a2a6a59
Dennis Kaselow <dennis.kaselow+git@gmail.com>
2023-12-01 07:15:39 +0100

    2023 day 01

--- lib/src/2023/day01.dart
+++ lib/src/2023/day01.dart
@@ -1,7 +1,60 @@
 import '../utils.dart';
 
-Iterable<String> _processInput(String input) => input.lines;
+final digitWords = [
+  ('one', '1'),
+  ('two', '2'),
+  ('three', '3'),
+  ('four', '4'),
+  ('five', '5'),
+  ('six', '6'),
+  ('seven', '7'),
+  ('eight', '8'),
+  ('nine', '9'),
+];
 
-int day1star1(String input) => _processInput(input).length;
+int day1star1(String input) => input.lines
+    .map(
+      (e) => e
+          .split('')
+          .where((element) => '123456789'.contains(element))
+          .map(int.parse),
+    )
+    .map((e) => e.first * 10 + e.last)
+    .reduce(sum);
 
-int day1star2(String input) => _processInput(input).length;
+int day1star2(String input) => input.lines
+    .map((e) {
+      var index = 999;
+      (String, String)? first;
+      (String, String)? last;
+      for (final digitWord in digitWords) {
+        final idx = e.indexOf(digitWord.$1);
+        if (idx != -1 && idx < index) {
+          first = digitWord;
+          index = idx;
+        }
+      }
+      index = -1;
+      for (final digitWord in digitWords) {
+        final idx = e.lastIndexOf(digitWord.$1);
+        if (idx > index) {
+          last = digitWord;
+          index = idx;
+        }
+      }
+      if (first != null) {
+        e = e.replaceFirst(first.$1, '_${first.$2}_${first.$1}');
+      }
+      if (last != null) {
+        e = e.replaceFirst(last.$1, '${last.$1}_${last.$2}_', index);
+      }
+      return e;
+    })
+    .map(
+      (e) => e
+          .split('')
+          .where((element) => '0123456789'.contains(element))
+          .map(int.parse),
+    )
+    .map((e) => e.first * 10 + e.last)
+    .reduce(sum);
