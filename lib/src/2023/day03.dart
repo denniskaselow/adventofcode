0064d65091c8a295415155919958c094ee00db1f
Dennis Kaselow <dennis.kaselow+git@gmail.com>
2023-12-03 09:14:12 +0100

    2023 day 03 - improved readability

--- lib/src/2023/day03.dart
+++ lib/src/2023/day03.dart
@@ -2,6 +2,8 @@ import 'package:collection/collection.dart';
 
 import '../utils.dart';
 
+typedef PartId = ({int line, int start, int end, int number});
+
 Iterable<String> _processInput(String input) => input.lines;
 
 int day03star1(String input) {
@@ -35,12 +37,11 @@ int day03star1(String input) {
 }
 
 int day03star2(String input) {
-  const validSymbol = '0123456789';
   const invalidSymbol = '0123456789.';
-  final data = <(int, int), (int, int, int, int)>{};
-  final lines = _processInput(input).indexed.toList();
+  final data = <(int, int), PartId>{};
+  final lines = _processInput(input).toList();
   final products = <int>[];
-  for (final (y, line) in lines) {
+  for (final (y, line) in lines.indexed) {
     final pattern = RegExp(r'(?<number>\d+)+');
     for (final match in pattern.allMatches(line)) {
       if (int.tryParse(match.namedGroup('number') ?? '') case final number?) {
@@ -52,38 +53,39 @@ int day03star2(String input) {
                 x + direction.x >= 0 &&
                 x + direction.x < line.length &&
                 !invalidSymbol
-                    .contains(input.lines[y + direction.y][x + direction.x])) {
+                    .contains(lines[y + direction.y][x + direction.x])) {
               isValid = true;
             }
           }
         }
         if (isValid) {
           for (var x = match.start; x < match.end; x++) {
-            data[(y, x)] = (y, match.start, match.end, number);
+            data[(y, x)] =
+                (line: y, start: match.start, end: match.end, number: number);
           }
         }
       }
     }
   }
 
-  for (final (y, line) in lines) {
+  const validSymbol = '0123456789';
+  for (final (y, line) in lines.indexed) {
     const gearPattern = '*';
     for (final match in gearPattern.allMatches(line)) {
-      final parts = <(int, int, int, int)>{};
+      final parts = <PartId>{};
       for (var x = match.start; x < match.end; x++) {
         for (final direction in DirectionSquare.values) {
           if (y + direction.y >= 0 &&
               y + direction.y < lines.length &&
               x + direction.x >= 0 &&
               x + direction.x < line.length &&
-              validSymbol
-                  .contains(lines[y + direction.y].$2[x + direction.x])) {
+              validSymbol.contains(lines[y + direction.y][x + direction.x])) {
             parts.add(data[(y + direction.y, x + direction.x)]!);
           }
         }
       }
       if (parts.length == 2) {
-        products.add(parts.first.$4 * parts.last.$4);
+        products.add(parts.first.number * parts.last.number);
       }
     }
   }
