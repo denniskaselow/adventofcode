c22dcee450a23738d7f9b9c560989ce0d870be16
Dennis Kaselow <dennis.kaselow+git@gmail.com>
2024-12-07 07:06:46 +0100

    2024 day 07 - scratch that, if you use the bridge right, you can absolutely stay DRY

--- lib/src/2024/day07.dart
+++ lib/src/2024/day07.dart
@@ -10,134 +10,67 @@ List<({int result, List<int> values})> _processInput(Input input) =>
             r'(\d+)',
           ).allMatches(values).map((match) => int.parse(match[1]!)).toList();
 
-      return (result: int.parse(result), values: value);
+      return (result: int.parse(result), values: value.reversed.toList());
     }).toList();
 
 int day07star1(Input input) =>
     _processInput(input)
         .where(
-          (element) =>
-              _validAddition(
-                element.result,
-                0,
-                element.values.reversed.toList(),
-              ) ||
-              _validMultiplication(
-                element.result,
-                1,
-                element.values.reversed.toList(),
-              ),
+          (element) => _validOperation(
+            element.result,
+            element.values.removeLast(),
+            element.values.toList(),
+          ),
         )
         .map((element) => element.result)
         .sum;
 
-bool _validAddition(
+bool _validOperation(
   int targetResult,
   int currentResult,
   List<int> remaining, {
   bool withConcat = false,
 }) {
   final current = remaining.removeLast();
-  final addResult = currentResult + current;
-  if (addResult > targetResult) {
-    return false;
-  }
+  final results = [
+    currentResult + current,
+    currentResult * current,
+    if (withConcat) int.parse('$currentResult$current'),
+  ];
   if (remaining.isEmpty) {
-    if (addResult == targetResult) {
-      return true;
+    for (final result in results) {
+      if (result == targetResult) {
+        return true;
+      }
     }
     return false;
   }
-  return _validAddition(
-        targetResult,
-        addResult,
-        remaining.toList(),
-        withConcat: withConcat,
-      ) ||
-      _validMultiplication(
-        targetResult,
-        addResult,
-        remaining.toList(),
-        withConcat: withConcat,
-      ) ||
-      (withConcat && _validConcat(targetResult, addResult, remaining));
-}
-
-bool _validMultiplication(
-  int targetResult,
-  int currentResult,
-  List<int> remaining, {
-  bool withConcat = false,
-}) {
-  final current = remaining.removeLast();
-  final multResult = currentResult * current;
-  if (multResult > targetResult) {
-    return false;
-  }
-  if (remaining.isEmpty) {
-    if (multResult == targetResult) {
-      return true;
+  for (final result in results) {
+    if (result > targetResult) {
+      return false;
     }
-    return false;
-  }
-  return _validAddition(
-        targetResult,
-        multResult,
-        remaining.toList(),
-        withConcat: withConcat,
-      ) ||
-      _validMultiplication(
-        targetResult,
-        multResult,
-        remaining.toList(),
-        withConcat: withConcat,
-      ) ||
-      (withConcat && _validConcat(targetResult, multResult, remaining));
-}
-
-bool _validConcat(int targetResult, int currentResult, List<int> remaining) {
-  final current = remaining.removeLast();
-  final concatResult = int.parse('$currentResult$current');
-  if (concatResult > targetResult) {
-    return false;
-  }
-  // print('$concatResult = $currentResult * $current');
-  if (remaining.isEmpty) {
-    if (concatResult == targetResult) {
-      return true;
+    final isValid = _validOperation(
+      targetResult,
+      result,
+      remaining.toList(),
+      withConcat: withConcat,
+    );
+    if (isValid) {
+      return isValid;
     }
-    return false;
   }
-  return _validAddition(
-        targetResult,
-        concatResult,
-        remaining.toList(),
-        withConcat: true,
-      ) ||
-      _validMultiplication(
-        targetResult,
-        concatResult,
-        remaining.toList(),
-        withConcat: true,
-      ) ||
-      _validConcat(targetResult, concatResult, remaining.toList());
+  return false;
 }
 
 int day07star2(Input input) =>
     _processInput(input)
         .where(
-          (element) =>
-              _validAddition(
-                element.result,
-                0,
-                element.values.reversed.toList(),
-              ) ||
-              _validMultiplication(
-                element.result,
-                1,
-                element.values.reversed.toList(),
-              ) ||
-              _validConcat(element.result, 0, element.values.reversed.toList()),
+          (element) => _validOperation(
+            element.result,
+            element.values.removeLast(),
+            element.values.toList(),
+            withConcat: true,
+          ),
         )
         .map((element) => element.result)
         .sum;
