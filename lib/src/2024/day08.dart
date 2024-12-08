fe9a3f74baabce132d95dd3dfc6813801c993458
Dennis Kaselow <dennis.kaselow+git@gmail.com>
2024-12-08 06:49:14 +0100

    2024 day 08 - the mind control Easter bunny (per)mutates

---
+++ lib/src/2024/day08.dart
@@ -0,0 +1,60 @@
+import 'package:more/more.dart' hide IndexedIterableExtension;
+
+import '../utils.dart';
+
+Map<String, List<({int x, int y})>> _processInput(Input input) => input
+    .getLines()
+    .indexed
+    .fold(<String, List<({int x, int y})>>{}, (antennas, line) {
+      line.$2.split('').indexed.where((cell) => cell.$2 != '.').forEach((cell) {
+        antennas.update(
+          cell.$2,
+          (value) => value..add((x: cell.$1, y: line.$1)),
+          ifAbsent: () => [(x: cell.$1, y: line.$1)],
+        );
+      });
+      return antennas;
+    });
+
+int day08star1(Input input) {
+  final antennas = _processInput(input);
+  final antinodes = <({int x, int y})>{};
+  for (final value in antennas.values) {
+    for (final [first, second] in value.permutations(2)) {
+      final diffX = second.x - first.x;
+      final diffY = second.y - first.y;
+      antinodes
+        ..add((x: first.x - diffX, y: first.y - diffY))
+        ..add((x: second.x + diffX, y: second.y + diffY));
+    }
+  }
+  final size = input.getLines().length;
+  return antinodes.count((element) => _isInGrid(element, size));
+}
+
+int day08star2(Input input) {
+  final antennas = _processInput(input);
+  final antinodes = <({int x, int y})>{};
+
+  final size = input.getLines().length;
+  for (final value in antennas.values) {
+    for (final [first, second] in value.permutations(2)) {
+      final diffX = second.x - first.x;
+      final diffY = second.y - first.y;
+      for (var i = 0; i < size; i++) {
+        final newAntinodes = [
+          (x: first.x - i * diffX, y: first.y - i * diffY),
+          (x: second.x + i * diffX, y: second.y + i * diffY),
+        ].where((antinode) => _isInGrid(antinode, size));
+        if (newAntinodes.isEmpty) {
+          break;
+        }
+        newAntinodes.forEach(antinodes.add);
+      }
+    }
+  }
+  return antinodes.length;
+}
+
+bool _isInGrid(({int x, int y}) element, int size) =>
+    element.x >= 0 && element.x < size && element.y >= 0 && element.y < size;
