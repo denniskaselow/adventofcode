879fc25ff31d184d832037ffe3161141cd20d601
Dennis Kaselow <dennis.kaselow+git@gmail.com>
2024-12-11 07:54:40 +0100

    2024 day 11 - removed some unnecessary variables

--- lib/src/2024/day11.dart
+++ lib/src/2024/day11.dart
@@ -10,24 +10,21 @@ int day11star1(Input input) => _blink(input, 25);
 int day11star2(Input input) => _blink(input, 75);
 
 int _blink(Input input, int times) {
-  var current = _processInput(input).toSet();
-  var currentCount = <String, int>{for (final value in current) value: 1};
+  var currentCount = <String, int>{
+    for (final value in _processInput(input)) value: 1,
+  };
   var nextCount = <String, int>{};
-  var next = <String>{};
-  final cache = <String, List<String>>{};
 
   for (var i = 0; i < times; i++) {
-    for (final element in current) {
-      final converted =
-          cache[element] ??= switch (element) {
-            '0' => ['1'],
-            _ when element.length.isEven => [
-              '${int.parse(element.substring(0, element.length ~/ 2))}',
-              '${int.parse(element.substring(element.length ~/ 2))}',
-            ],
-            _ => ['${int.parse(element) * 2024}'],
-          };
-      next.addAll(converted);
+    for (final element in currentCount.keys) {
+      final converted = switch (element) {
+        '0' => ['1'],
+        _ when element.length.isEven => [
+          '${int.parse(element.substring(0, element.length ~/ 2))}',
+          '${int.parse(element.substring(element.length ~/ 2))}',
+        ],
+        _ => ['${int.parse(element) * 2024}'],
+      };
       for (final key in converted) {
         nextCount.update(
           key,
@@ -37,9 +34,7 @@ int _blink(Input input, int times) {
       }
     }
 
-    (current, next) = (next, current);
     (currentCount, nextCount) = (nextCount, currentCount);
-    next.clear();
     nextCount.clear();
   }
 
